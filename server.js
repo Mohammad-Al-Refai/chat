87a1fa2602ee8929a9fd015d8b5ead41ae1ba335
Mohammad-AL-Rifai <55941955+Mohammad-Al-Refai@users.noreply.github.com>
2020-03-06 20:57:41 +0200

    new

---
+++ server.js
@@ -0,0 +1,33 @@
+let express=require("express")
+let app=express()
+let WS=require("express-ws")(app)
+app.use(express.static(__dirname+"/view"))
+
+app.get("/",(req,res)=>{
+    res.render(__dirname+"/view/index.html")
+})
+
+
+app.ws('/chat', function(ws, req) {
+    ws.on('message', function(msg) {
+        let data=JSON.parse(msg)
+        if(data.type=="name"){
+            ws.personName=data.data
+            console.log( ws.personName)
+            }
+            WS.getWss().clients.forEach(function client(A){
+                if(ws!=A){
+                    A.send(JSON.stringify({
+                     name:A.personName,
+                     msg:data.msg
+                     }))
+                }
+            })
+
+        
+})
+})
+
+
+
+app.listen(3000)
