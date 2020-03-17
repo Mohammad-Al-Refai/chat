9f468dadbfd86aac0668c26f5186cd4362ed975f
Mohammad-AL-Rifai <55941955+Mohammad-Al-Refai@users.noreply.github.com>
2020-03-17 22:32:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
-let express=require("express")
-let app=express()
-let WS=require("express-ws")(app)
-app.use(express.static(__dirname+"/view"))
+let express = require("express");
+let app = express();
+let WS = require("express-ws")(app);
+app.use(express.static(__dirname + "/view"));
 let port=process.env.PORT||3000
-app.ws('/', function(ws, req) {
-    ws.on('message', function(msg) {
-           ws.send("GOOD")
-            
-//             WS.getWss().clients.forEach(function client(A){
+
+app.ws("/chat", function(ws, req) {
+  ws.on("message", function(msg) {
+    let V = JSON.parse(msg);
+    if (V.type == "name") {
+      ws.personName = V.data;
+    }
+    WS.getWss().clients.forEach(function client(A) {
+    //   console.log("CLIENTS:", A.personName);
+
+      if (A.personName ===V.for) {
+        A.send(
+          JSON.stringify({
+            name: A.personName,
+            msg: V.msg
+          })
+        );
+        
+      }else{
+          console.log("TRG Client:",V.for)
+      }
+    });
+  });
+});
+
+app.listen(port);
+
+// WS.getWss().clients.forEach(function client(A){
 //                 if(ws!=A){
 //                     A.send(JSON.stringify({
 //                      name:A.personName,
@@ -15,11 +38,3 @@ app.ws('/', function(ws, req) {
 //                      }))
 //                 }
 //             })
-
-        
-})
-})
-
-
-
-app.listen(port)
