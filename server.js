a7a48d0f7384d001c0bc359b6ad19ec2cee031dc
Mohammad-AL-Rifai <55941955+Mohammad-Al-Refai@users.noreply.github.com>
2020-03-17 17:56:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,26 +4,19 @@ let WS=require("express-ws")(app)
 app.use(express.static(__dirname+"/view"))
 let port=process.env.PORT||3000
 
-app.get("/",(req,res)=>{
-    res.render(__dirname+"/view/index.html")
-})
 
-
-app.ws('/chat', function(ws, req) {
+app.ws('/', function(ws, req) {
     ws.on('message', function(msg) {
-        let data=JSON.parse(msg)
-        if(data.type=="name"){
-            ws.personName=data.data
-            console.log( ws.personName)
+           ws.send("GOOD")
             }
-            WS.getWss().clients.forEach(function client(A){
-                if(ws!=A){
-                    A.send(JSON.stringify({
-                     name:A.personName,
-                     msg:data.msg
-                     }))
-                }
-            })
+//             WS.getWss().clients.forEach(function client(A){
+//                 if(ws!=A){
+//                     A.send(JSON.stringify({
+//                      name:A.personName,
+//                      msg:data.msg
+//                      }))
+//                 }
+//             })
 
         
 })
