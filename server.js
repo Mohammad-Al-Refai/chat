804a20a9c1098e8e1f8f70fbf67b3b73442317d8
Mohammad-AL-Rifai <55941955+Mohammad-Al-Refai@users.noreply.github.com>
2020-03-17 17:59:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ let app=express()
 let WS=require("express-ws")(app)
 app.use(express.static(__dirname+"/view"))
 let port=process.env.PORT||3000
-
-
 app.ws('/', function(ws, req) {
     ws.on('message', function(msg) {
            ws.send("GOOD")
-            }
+            
 //             WS.getWss().clients.forEach(function client(A){
 //                 if(ws!=A){
 //                     A.send(JSON.stringify({
