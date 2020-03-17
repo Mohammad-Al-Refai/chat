0f6b51ad2ec695dfd69002961230a58bd7d0d883
Mohammad-AL-Rifai <55941955+Mohammad-Al-Refai@users.noreply.github.com>
2020-03-18 01:30:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,24 +7,25 @@ let port=process.env.PORT||3000
 app.ws("/chat", function(ws, req) {
   ws.send('Welcome')
   ws.on("message", function(msg) {
+    ws.send('Welcome')
     let V = JSON.parse(msg);
     if (V.type == "name") {
       ws.personName = V.data;
     }
     WS.getWss().clients.forEach(function client(A) {
     //   console.log("CLIENTS:", A.personName);
-
-      if (A.personName ===V.for) {
-        A.send(
-          JSON.stringify({
-            name: ws.personName,
-            msg: V.msg
-          })
-        );
+        A.send("HI")
+//       if (A.personName ===V.for) {
+//         A.send(
+//           JSON.stringify({
+//             name: ws.personName,
+//             msg: V.msg
+//           })
+//         );
         
-      }else{
-          console.log("TRG Client:",V.for)
-      }
+//       }else{
+//           console.log("TRG Client:",V.for)
+//       }
     });
   });
 });
